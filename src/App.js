0a63f50c362783480b6ea4182e1a7f611d82b53e
sajberpapak <sajberpapak@yandex.com>
2019-03-12 09:13:17 +0100

    Added Components and setup routes with React Router

--- src/App.js
+++ src/App.js
@@ -4,16 +4,41 @@
   // - Added Google Fonts
   // - Added Font Awesome
 
+// Components setup
+  // App
+  // Navbar
+  // ProductList
+    // Product
+  // Details
+  // Cart
+  // Default
+
+// Install React Router via npm: npm i --save react-router-dom
+// {BrowserRouter, Switch, Route, Link}
+
 import React, { Component } from 'react';
+import {Switch, Route} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
+import Navbar from "./components/Navbar"
+import ProductList from "./components/ProductList";
+import Cart from "./components/Cart";
+import Default from "./components/Default";
+import Details from "./components/Details";
+
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <h3>Hello from App</h3>
+        <Navbar/>
+        <Switch>
+          <Route exact path="/" component={ProductList}></Route>
+          <Route path="/details" component={Details}></Route>
+          <Route path="/cart" component={Cart}></Route>
+          <Route component={Default}></Route>
+        </Switch>
       </React.Fragment>
     );
   }
