b7ecbce4316f37384c4da14cc0faed37cc1a6e5b
sajberpapak <sajberpapak@yandex.com>
2019-03-14 10:10:36 +0100

    Resolve all the errors considering context.js and id propetry

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,11 +1,69 @@
 import React, {Component} from 'react';
+import {ProductConsumer} from "../context";
+import {Link} from "react-router-dom";
+import {ButtonContainer} from "./Button";
+
+
 
 class Details extends Component {
   render() {
     return (
-      <div>
-        <h3>Hello from Details</h3>
-      </div>
+      <ProductConsumer>
+        {(value)=>{
+          // console.log(value.detailProduct);
+          // Destructuring
+          const {id, company, img, info, price, title, inCart} = value.detailProduct;
+
+          return(
+            <div className="container py-5">
+              {/*title*/}
+              <div className="row">
+                <div className="col-10 mx-auto text-center">
+                  <h1>{title}</h1>
+                </div>
+              </div>
+            {/*end of title*/}
+
+            {/*product info*/}
+
+            <div className="row">
+              <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+                <img src={img} alt="product" className="img-fluid"/>
+              </div>
+
+              {/*product text*/}
+              <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+                <h2>model: {title}</h2>
+                <h4>mady by: <span className="text-uppercase">{company}</span></h4>
+                <h4><strong>price: <span>$</span>{price}</strong></h4>
+                <p>some info</p>
+                <p>{info}</p>
+                {/*buttons*/}
+                <div>
+                  <Link to="/">
+                    <ButtonContainer>
+                      back to products
+                    </ButtonContainer>
+                  </Link>
+
+                  <ButtonContainer disabled={inCart ? true : false} onClick={()=>{
+                    value.addToCart(id);
+                  }}
+
+                  >
+                    {inCart ? "inCart" : "add to cart"}
+                  </ButtonContainer>
+                </div>
+              </div>
+
+            </div>
+
+            </div>
+          )
+
+
+        }}
+      </ProductConsumer>
     );
   }
 }
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,13 +1,40 @@
 import React, {Component} from 'react';
 import Product from "./Product";
-
+import Title from "./Title";
+import {storeProducts, detailProduct} from "../data";
+import {ProductConsumer} from "../context";
 
 class ProductList extends Component {
+
+
   render() {
+
     return (
       <div>
 
-        <Product/>
+        <React.Fragment>
+          <div className="py-5">
+            <div className="container">
+              <Title name="our" title="products"/>
+              {/*Products row*/}
+              <div className="row">
+                <ProductConsumer>
+                  {(value)=>{
+                    // console.log(value);
+                    // console.log(value.products[1].id);
+
+                      return value.products.map(product => {
+                      return <Product key={product.id} product={product}/>
+                    })
+                  }}
+                </ProductConsumer>
+              </div>
+            </div>
+          </div>
+
+        </React.Fragment>
+
+
       </div>
     );
   }
