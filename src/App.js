2467b31d20796c18b2e44af891ec46269d05b4e3
sajberpapak <sajberpapak@yandex.com>
2019-03-15 11:07:22 +0100

    fixed some small issues

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ import ProductList from "./components/ProductList";
 import Cart from "./components/Cart";
 import Default from "./components/Default";
 import Details from "./components/Details";
-
+import Modal from "./components/Modal"
 
 class App extends Component {
   render() {
@@ -38,6 +38,7 @@ class App extends Component {
           <Route path="/cart" component={Cart}></Route>
           <Route component={Default}></Route>
         </Switch>
+        <Modal/>
       </React.Fragment>
     );
   }
